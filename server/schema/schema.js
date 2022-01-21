fdedc5c7851f59bf537d37671cc8e0712689a43e
jaymesC <clementjaymes@gmail.com>
2022-01-20 20:44:30 -0800

    update schema.js

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -17,7 +17,7 @@ let books = [
   { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2",},
   { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3",},
   { name: "The Hero of Ages", genre: "Fantasy", id: "4", authorId: "2",},
-  { name: "The colour of Magic", genre: "Fantasy", id: "5", authorId: "3",},
+  { name: "The Colour of Magic", genre: "Fantasy", id: "5", authorId: "3",},
   { name: "The Light Fantastic", genre: "Fantasy", id: "6", authorId: "3",},
 ];
 
